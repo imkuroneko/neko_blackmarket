ea99bcfbdb9a063b4e3e63d146ce70658f696057
KuroNeko <20273059+imkuroneko@users.noreply.github.com>
2024-03-18 02:17:28 -0300

    ⚙️ Fix generación coord

--- server.lua
+++ server.lua
@@ -1,8 +1,17 @@
 local ox_inventory = exports.ox_inventory
+local coords = nil
 
-lib.callback.register('neko_blackmarket:server:getPedLocation', function(source, cb)
-    return Config.Locations[math.random(#Config.Locations)]
+CreateThread(function()
+    coords = Config.Locations[math.random(#Config.Locations)]
 end)
+
+lib.callback.register('neko_blackmarket:server:getPedLocation', function()
+    return coords
+end)
+
+function GetPedCoords()
+    return coords
+end
 
 for key, value in pairs(Config.Categories) do
     ox_inventory:RegisterShop('neko_blackmarket_'..key, {
